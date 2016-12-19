87be20d02fc5c286e7a826f1a12ccd1ff6b89abc
Sam Atkins <sam@cubi.io>
2016-12-19 10:47:57 +0000

    chore: Update build tasks and config

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -8,7 +8,7 @@ const rsync = require('rsyncwrapper').rsync;
 const config = require('../config.js');
 
 // require creds
-const creds =  JSON.parse(fs.readFileSync('../../secrets.json'));
+// const creds =  require('../../secrets.json');
 
 // run dist in local server
 gulp.task('serve:dist', function() {
@@ -22,27 +22,27 @@ gulp.task('serve:dist', function() {
 })
 
 // rsync task
-gulp.task('rsync', function() {
-	rsync({
-		src: 'dist/',
-		// keep dest in secrets.json
-		dest: creds.dest,
-		ssh: true,
-		recursive: true,
-		deleteAll: true
-	}, function(error, stdout, stderr, cmd) {
-		if (error) {
-			console.log(error.message);
-			console.log(stdout);
-			console.log(stderr);
-		}
-	});
-})
+// gulp.task('rsync', function() {
+// 	rsync({
+// 		src: 'dist/',
+// 		// keep dest in secrets.json
+// 		dest: creds.dest,
+// 		ssh: true,
+// 		recursive: true,
+// 		deleteAll: true
+// 	}, function(error, stdout, stderr, cmd) {
+// 		if (error) {
+// 			console.log(error.message);
+// 			console.log(stdout);
+// 			console.log(stderr);
+// 		}
+// 	});
+// })
 
 // deploy to surge.sh
 gulp.task('surge', [], function () {
   return surge({
-  	config.surge.project,
-  	config.surge.domain
+ 		project:	config.surge.project,
+  	domain:	config.surge.domain
   })
 })
