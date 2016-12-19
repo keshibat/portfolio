ad7d1a0ba859620ae3d90552651289e936f3e0d7
Sam Atkins <sam@cubi.io>
2016-12-19 10:48:47 +0000

    feat: Add responsive menu (basic version)

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,17 @@
 // use strict
 'use strict';
+
+/*
+ * Open the drawer when the menu icon is clicked
+ */
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
