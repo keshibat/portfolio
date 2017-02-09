23cf6b6b88a70818758e622b565cf31824f8f4b9
Sam Atkins <sam@cubi.io>
2017-02-09 16:51:14 +0000

    style: Remove commented out code; no code change

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,3 @@
-// use strict
-// 'use strict';
-
-/**
-    * TODO
-    *
-    * => refactor below into Knockout.js  
-*/
-
-/*
- * Open the drawer when the menu icon is clicked
- */
-// var menu = document.querySelector('#menu');
-// var main = document.querySelector('main');
-// var drawer = document.querySelector('.nav');
-
-// menu.addEventListener('click', function(e) {
-//   drawer.classList.toggle('open');
-//   e.stopPropagation();
-// });
-// main.addEventListener('click', function() {
-//   drawer.classList.remove('open');
-// });
-
-/* ==========================================================================
-    Knockout
-========================================================================== */
-
 var portfolioModals = [
     {
         portfolioImage: "images/python_blog_medium.png",
